d0c0e61d375c0c8847a8298d31e578349fa548b1
youngtechwiz-lgtm <youngtechwiz@gmail.com>
2026-08-05 14:26:28 -0700

    Fix

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
+// main.jsx
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
-import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={import.meta.env.BASE_URL}>
       <App />
     </BrowserRouter>
   </React.StrictMode>
